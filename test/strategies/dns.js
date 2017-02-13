986d7691ac83db880973eac0dcdc6b7d666b0b0e
Coen Hyde <coen.hyde@gmail.com>
2017-02-12 21:43:29 -0800

    fixing bug on domains that return no TXT records

--- test/strategies/dns.js
+++ test/strategies/dns.js
@@ -7,36 +7,59 @@ var dns = require('dns');
 var dnstxt = require('../../lib/strategies/dnstxt');
 
 describe('Strategies:DNSTXT', function() {
-  var stubbedDNS = null;
   var token='wef0vihqw3rpoaxufg';
 
-  beforeEach(function() {
-    stubbedDNS = sinon.stub(dns, 'resolveTxt', (domain, cb) => {
-      return cb(null, [
-        [ 'v=spf1 include:_spf.google.com -all' ],
-        [ `cronio=${token}` ],
-        [ `random txt record` ]
-      ]);
+  describe('for domains without TXT records', function() {
+    var stubbedDNS = null;
+
+    beforeEach(function() {
+      stubbedDNS = sinon.stub(dns, 'resolveTxt', (domain, cb) => cb(null, undefined));
+    });
+
+    afterEach(function() {
+      stubbedDNS.restore();
+    })
+
+    it('should not return verified', function(done) {
+      dnstxt('example.com', "bla", function(err, verified) {
+        expect(err).to.equal(null);
+        expect(verified).to.equal(false);
+        done();
+      });
     });
   });
 
-  afterEach(function() {
-    stubbedDNS.restore();
-  })
+  describe('for domains with TXT records', function() {
+    var stubbedDNS = null;
 
-  it('should return not verified for search of TXT DNS records with bad token', function(done) {
-    dnstxt('popbasic.com', "bla", function(err, verified) {
-      expect(err).to.equal(null);
-      expect(verified).to.equal(false);
-      done();
+    beforeEach(function() {
+      stubbedDNS = sinon.stub(dns, 'resolveTxt', (domain, cb) => {
+        return cb(null, [
+          [ 'v=spf1 include:_spf.google.com -all' ],
+          [ `cronio=${token}` ],
+          [ `random txt record` ]
+        ]);
+      });
+    });
+
+    afterEach(function() {
+      stubbedDNS.restore();
+    })
+
+    it('should return not verified for search of TXT DNS records with bad token', function(done) {
+      dnstxt('example.com', "bla", function(err, verified) {
+        expect(err).to.equal(null);
+        expect(verified).to.equal(false);
+        done();
+      });
+    });
+
+    it('should return verified for search of TXT DNS records with correct token', function(done) {
+      dnstxt('example.com', `cronio=${token}`, function(err, verified) {
+        expect(err).to.equal(null);
+        expect(verified).to.equal(true);
+        done();
+      });
     });
   });
-
-  it('should return verified for search of TXT DNS records with correct token', function(done) {
-    dnstxt('popbasic.com', `cronio=${token}`, function(err, verified) {
-      expect(err).to.equal(null);
-      expect(verified).to.equal(true);
-      done();
-    });
-  })
 })
